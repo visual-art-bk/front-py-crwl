10595f9fe7bde9f1c2b420fa3ac1f35425a466a0
kbk <kekekj@naver.com>
2024-11-12 15:15:06 +0900

    메인 타이틀 상태 초기화

--- src/AppContainer/Crawler/MainTitle/MainTitle.tsx
+++ src/AppContainer/Crawler/MainTitle/MainTitle.tsx
@@ -24,20 +24,23 @@ export default function MainTitle(props: tMainTitleProps) {
   }, [isShowData]);
 
   useEffect(() => {
-    let index = 0;
-    const delay = 100; // 글자 타이핑 속도 (ms)
+    if (!isShowData) {
+      setDisplayedText(""); // 상태 초기화
+      let index = 0;
+      const delay = 100; // 글자 타이핑 속도 (ms)
 
-    const intervalId = setInterval(() => {
-      if (index <= fullText.length) { // 텍스트 길이를 초과하지 않도록 조건 추가
-        setDisplayedText(fullText.slice(0, index)); // index까지의 부분 문자열 설정
-        index++;
-      } else {
-        clearInterval(intervalId);
-      }
-    }, delay);
+      const intervalId = setInterval(() => {
+        if (index <= fullText.length) { // 텍스트 길이를 초과하지 않도록 조건 추가
+          setDisplayedText(fullText.slice(0, index)); // index까지의 부분 문자열 설정
+          index++;
+        } else {
+          clearInterval(intervalId);
+        }
+      }, delay);
 
-    return () => clearInterval(intervalId); // 컴포넌트 언마운트 시 타이머 정리
-  }, []);
+      return () => clearInterval(intervalId); // 컴포넌트 언마운트 시 타이머 정리
+    }
+  }, [isShowData]); // isShowData의 상태 변경 시 실행
 
   return (
     <div className={classNameOfMainTitle}>
